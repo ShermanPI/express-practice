08077c873cf34212a33eb6b49abda6d002144bde
Sherman Perez <shermanperez0@gmail.com>
2024-09-19 02:51:03 -0400

    client can filter by genre

--- ExpressRESTApi/app.mjs
+++ ExpressRESTApi/app.mjs
@@ -1,5 +1,5 @@
 import express from 'express'
-import moviesData from './moviesMock.json' assert {type: "json"}
+import moviesData from './moviesMock.json' assert {type: 'json'}
 
 const app = express()
 const PORT = 1234
@@ -7,22 +7,27 @@ const PORT = 1234
 app.disable('x-powered-by')
 
 app.get('/', (req, res) => {
-    console.log(moviesData, '🐕🐕🐕🐕')
-    res.send({ message: 'This is an API to learn ExpressJs :D' })
+  res.send({ message: 'This is an API to learn ExpressJs :D' })
 })
 
 app.get('/movies', (req, res) => {
-    res.send(moviesData)
+  const { genre } = req.query
+
+  if (genre) {
+    const data = moviesData.filter(movie => movie.genre.map(genre => genre.toLocaleLowerCase()).includes(genre))
+    res.json(data)
+  }
+  res.send(moviesData)
 })
 
 app.get('/movies/:id', (req, res) => {
-    const { id } = req.params
-    const movie = moviesData.find(movie => movie.id === id)
-    if (movie) return res.json(movie)
-        
-    res.status(404).json({ message: 'Movie not found' })
+  const { id } = req.params
+  const movie = moviesData.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+
+  res.status(404).json({ message: 'Movie not found' })
 })
 
 app.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`)
+  console.log(`Server listening on http://localhost:${PORT}`)
 })
